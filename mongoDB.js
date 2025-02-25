bf3578343983cb9d256480801b0198e7c433c76a
darshan-028 <darshanrajpatil10@gmail.com>
2025-02-25 19:15:50 +0530

    database connection
    mongodb is connected, fileSchema and userSchema are created.

---
+++ mongoDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',()=> console.log("DataBase is connected"));
+
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
+ 
+}
+
+
+
+export default connectDB; 
